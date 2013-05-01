720a8c86f0a256473575ae06d2f1d0c3ee895d57
Adrián Casimiro Álvarez <adrian.casimiro.alvarez@gmail.com>
2013-05-01 15:44:43 +0200

    Crossover finished

--- src/es/unileon/rnag/operator/crossover/SinglePoint.java
+++ src/es/unileon/rnag/operator/crossover/SinglePoint.java
@@ -1,6 +1,9 @@
 package es.unileon.rnag.operator.crossover;
 
+import java.util.Random;
+
 import es.unileon.rnag.chromosome.Chromosome;
+import es.unileon.rnag.gen.Gen;
 
 /**
  * Class that make the Single Point chrossover
@@ -11,12 +14,30 @@ public class SinglePoint extends CrossoverStrategy{
 
 	@Override
 	public CrossoverElement doCrossover(CrossoverElement crossoverElement) {
-		Chromosome firstChromosome = crossoverElement.getFirstChromosome();
-		Chromosome secondChromosome = crossoverElement.getSecondChromosome();
+		Random ran = new Random();
+		double probability = ran.nextDouble();
 		
+		if(probability < this.getCrossoverProbability()){
+			Chromosome firstChromosome = crossoverElement.getFirstChromosome();
+			Chromosome secondChromosome = crossoverElement.getSecondChromosome();
+			
+			int length = firstChromosome.length();
+			
+			int position = ran.nextInt(length);
+			
+			Gen[] sliceFirstChromosome = firstChromosome.getChromosomeSlice(0, position);
+			Gen[] sliceSecondChromosome = secondChromosome.getChromosomeSlice(0, position);
+			
+			firstChromosome.setChromosomeSlice(sliceSecondChromosome, 0);
+			secondChromosome.setChromosomeSlice(sliceFirstChromosome, 0);
+			
+			CrossoverElement result = new CrossoverElement(firstChromosome, secondChromosome);
 		
-		
-		return null;
+			return result;
+		}
+		else{
+			return crossoverElement;
+		}
 	}
 
 }
